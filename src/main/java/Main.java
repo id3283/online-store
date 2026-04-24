d8c8c957952aac481c997369bdf49712f4a8df74
Dave Dykes <148535937+brightworkdigital@users.noreply.github.com>
2026-04-24 15:02:17 +0100

    Added text to main menu prompt.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Main {
+    Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        mainMenu();
+    }
+
+    private static void mainMenu() {
+        String prompt = """
+                1. Menu option one
+                
+                
+                """;
+    }
 }
