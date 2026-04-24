d9450bd7256ae8a4cdafa6e482c4c59b2d35b72a
Dave Dykes <148535937+brightworkdigital@users.noreply.github.com>
2026-04-24 15:24:46 +0100

    added submenus and prompts

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,83 @@
 import java.util.Scanner;
 
 public class Main {
-    Scanner scanner = new Scanner(System.in);
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         mainMenu();
+        System.out.println("Thanks for using my app.  Bye!!");
     }
 
     private static void mainMenu() {
+        String userInput;
         String prompt = """
-                1. Menu option one
+                        Main Menu
+                    1. Search for product submenu
+                    2. Display all products
+                    3. You guessed it, option three
+                    X. Exit the application
+                    
+                    Select one of the options above
+                    """;
+        boolean done = false;
+        do {
+            System.out.println(prompt);
+            userInput = scanner.nextLine();
+
+            switch (userInput) {
+                case "1":
+                    searchMenu();
+                    break;
+                case "2":
+                    displayProducts();
+                    break;
+                case "3":
+                    done = true;
+                default:
+                    System.err.println("Invalid input.  Please try again.");
+            }
+        } while(!done);
+    }
+
+    private static void displayProducts() {
+        String userInput;
+        String prompt = """
+                    Search Menu
+                1. Search by product name
+                2. Search by price
+                X. Exit search menu
                 
-                
+                Select one of the options above
                 """;
+        boolean done = false;
+        do {
+            System.out.println(prompt);
+            userInput = scanner.nextLine();
+
+            switch (userInput) {
+                case "1":
+                    searchByName();
+                    break;
+                case "2":
+                    searchByPrice();
+                    break;
+                case "3":
+                    done = true;
+                default:
+                    System.err.println("Invalid input.  Please try again.");
+            }
+        } while(!done);
+    }
+
+    private static void searchByPrice() {
+
+    }
+
+    private static void searchByName() {
+
+    }
+
+    private static void searchMenu() {
+
     }
 }
